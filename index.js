d3c1ab2bff7bd91ce2a8c029a8c56a31dbe03936
Dewan Ahmed <dewan.ishtiaque@hotmail.com>
2019-07-19 11:20:54 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World from ' + req.protocol + '://' + req.get('host') + req.originalUrl+'\n*****Version: 0.0.1*****');
+});
+
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+
+  console.log('Your nodejs app is listening at port %s',  port);
+});
