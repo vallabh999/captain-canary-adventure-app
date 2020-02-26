db78debe789074ef9f202e755f982c0eb524213d
dewandemo <61483131+dewandemo@users.noreply.github.com>
2020-02-25 23:37:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello DOTC2020 Auckland from ' + req.protocol + '://' + req.get('host') + req.originalUrl+'\n*****Version: 0.0.1*****');
+  res.send('
+╔╗─╔╗──╔╗╔╗────╔═══╗─────╔╗─╔╗────────╔╗
+║║─║║──║║║║────║╔═╗║─────║║─║║────────║║
+║╚═╝╠══╣║║║╔══╗║║─║╠╗╔╦══╣║╔╣║╔══╦═╗╔═╝║
+║╔═╗║║═╣║║║║╔╗║║╚═╝║║║║╔═╣╚╝╣║║╔╗║╔╗╣╔╗║
+║║─║║║═╣╚╣╚╣╚╝║║╔═╗║╚╝║╚═╣╔╗╣╚╣╔╗║║║║╚╝║
+╚╝─╚╩══╩═╩═╩══╝╚╝─╚╩══╩══╩╝╚╩═╩╝╚╩╝╚╩══╝');
 });
 
 var server = app.listen(3000, function () {
