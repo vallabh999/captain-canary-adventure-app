6534cd9244e8531c813035b3caf142ca4f2d90f7
dewandemo <61483131+dewandemo@users.noreply.github.com>
2023-12-19 18:51:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,43 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    const version = "1.0.0"; // Static version number
-    const serverUrl = req.protocol + '://' + req.get('host');
-    res.send(`Hello World!<br>Server URL: ${serverUrl}<br>Version: ${version}`);
+    const podName = process.env.HOSTNAME || 'Unknown';
+
+    res.send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Captain Canary's Adventures</title>
+            <script>
+                function toggleImage() {
+                    var img = document.getElementById('canaryImg');
+                    var btn = document.getElementById('toggleBtn');
+                    var text = document.getElementById('displayText');
+                    if (img.src.includes('canary-working.png')) {
+                        img.src = '/canary-vacation.png';
+                        btn.textContent = 'Back to Work!';
+                        text.innerHTML = "It's " + new Date().toLocaleTimeString('en-US', { timeZone: 'Pacific/Honolulu', hour: '2-digit', minute: '2-digit', hour12: true }) + " in Hawaii.<br>While you're stuck debugging, Captain Canary is catching waves and enjoying the Aloha spirit! 🏄‍♂️🌺";
+                    } else {
+                        img.src = '/canary-working.png';
+                        btn.textContent = 'Go on Vacation!';
+                        text.innerHTML = 'Pod Name: ${podName}';
+                    }
+                }
+            </script>
+        </head>
+        <body>
+            <img id="canaryImg" src="/canary-working.png" alt="Captain Canary Working" style="width:200px;">
+            <button id="toggleBtn" onclick="toggleImage()">Go on Vacation!</button>
+            <p id="displayText">Pod Name: ${podName}</p>
+        </body>
+        </html>
+    `);
 });
 
 app.listen(port, () => {
-    console.log(`Demo app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
